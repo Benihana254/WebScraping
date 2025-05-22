f3a5d98a73ad172db1e2d81a3e6080e893afc576
Wazakiri <tle439923@Students.mu.ac.ke>
2025-05-22 19:06:57 +0300

    downloading .txt files from the web

---
+++ requestModule.py
@@ -0,0 +1,13 @@
+# Downloads Romeo and Juliet and Saves it on the deskTop
+from os import makedirs
+import requests,shutil,os
+response = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+with open('Romeo', 'w', encoding='utf-8') as novel:
+    novel.write(response.text)
+    os.startfile('Romeo')
+old_path = (os.path.abspath('Romeo'))
+new_path = r'C:\Users\gh\Desktop\Romeo_and_Juliet'
+os.makedirs(new_path, exist_ok=True)
+shutil.move(old_path, new_path)
+print('Download Complete!')
+# os.startfile(.join(new_path,'Romeo.txt')
