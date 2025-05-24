7909ae257bf5c1391565c0399b9fac191d4fb223
Wazakiri <tle439923@Students.mu.ac.ke>
2025-05-24 09:54:04 +0300

    Implementing exception handling

--- requestModule.py
+++ requestModule.py
@@ -2,6 +2,12 @@
 from os import makedirs
 import requests,shutil,os
 response = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+# the next block attempt to catch any runtime errors
+try:
+    response.raise_for_status()
+except Exception as exc:
+    print(f'Sorry programme crashed error code; {exc}') # 'Exception' catches general error while exc catches the specific one
+
 with open('Romeo', 'w', encoding='utf-8') as novel:
     novel.write(response.text)
     os.startfile('Romeo')
